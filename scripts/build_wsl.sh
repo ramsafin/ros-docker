6b9c8ec5022c1c73cbc2121a8afd9103e3c8be2c
Ramil Safin <ramilsafnab1996@gmail.com>
2025-02-06 00:49:41 +0300

    Rename Dockerfile

--- scripts/build_wsl.sh
+++ scripts/build_wsl.sh
@@ -5,6 +5,6 @@ DOCKER_IMAGE="ros-noetic-wsl:latest"
 
 echo "Building ROS Noetic Docker image for WSL..."
 
-docker build --build-arg WSL=true -t $DOCKER_IMAGE -f docker/Dockerfile .
+docker build --build-arg WSL=true -t $DOCKER_IMAGE -f docker/Dockerfile.noetic .
 
 echo "Build completed: $DOCKER_IMAGE"
