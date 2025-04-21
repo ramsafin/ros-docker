f3dbf54f6d948f60d41ff7148a9fee76a10daa55
Ramil Safin <RamilSafNab1996@gmail.com>
2025-04-21 19:54:03 +0300

    Update run_linux.sh

--- scripts/run_linux.sh
+++ scripts/run_linux.sh
@@ -7,8 +7,13 @@ DOCKER_CONTAINER="ros-noetic-dev-linux"
 echo "Running Docker container for Linux..."
 
 docker run -it --rm \
+    --gpus all \
     --net=host \
+    --runtime=nvidia \
     -e DISPLAY \
+    -e WAYLAND_DISPLAY \
+    -e XDG_RUNTIME_DIR \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(pwd)/catkin_ws:/home/ros/catkin_ws \
     --name "${DOCKER_CONTAINER}" \
     $DOCKER_IMAGE
